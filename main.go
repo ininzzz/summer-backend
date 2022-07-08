3ffaa41f049e8aef9859d4077df534cd2891fd49
Yong-Sun-2001 <924466726@qq.com>
2022-07-08 19:21:26 +0800

    修改中间件使用时机

--- main.go
+++ main.go
@@ -37,15 +37,15 @@ func Register(r *gin.Engine) {
 	}
 	blogGroup := r.Group("/blog")
 	{
-		blogGroup.Use(utils.JwtAuth()) //需要登录的路由
-		{
-			blogGroup.POST("/post", web.BlogWebHandler.BlogPost) //发布帖子
-		}
 		// blogGroup.POST("/post", web.BlogWebHandler.BlogPost)           //发布帖子
 		blogGroup.GET("/home/list", web.BlogWebHandler.HomeList)       //查看首页帖子【基于滚动分页】
 		blogGroup.GET("/space/list", web.BlogWebHandler.SpaceList)     //获取某个用户发布的所有帖子【不分页】
 		blogGroup.GET("/info", web.BlogWebHandler.Info)                //获取某个帖子内容
 		blogGroup.GET("/comment/list", web.BlogWebHandler.CommentList) //获取某个帖子的所有评论信息
+		blogGroup.Use(utils.JwtAuth())                                 //需要登录的路由
+		{
+			blogGroup.POST("/post", web.BlogWebHandler.BlogPost) //发布帖子
+		}
 	}
 }
 
